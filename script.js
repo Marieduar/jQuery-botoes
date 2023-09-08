98151cbe12ea4e7aa898a950113dd958ba2b55f4
Maria Eduarda Aguirre <134699952+Marieduar@users.noreply.github.com>
2023-09-08 16:42:03 -0300

    ....

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    //  adicionar o codigo javascript - jquery dentro do documento
+
+    $('#btnCN').click(function(){
+        //acao do clique no elemento definido
+        alert('Em Ciências da Natureza estamos recentemente aprendendo os conteúdos de: Física: Movimento Retilíneo Uniforme ou MRU - Que é o movimento constante em uma velocidade em linha reta.                 Biologia: Membranas celulares - é a interface entre a célula e o ambiente, definindo os limite de uma célula e mantem as diferenças essenciais entre o citosol e o meio.                                                   Química: Funções inorgânicas: são substancias agrupadas em família, porque têm propriedades semelhantes.');
+    });
+
+    $('#btnCH').click(function(){
+        //acao do clique no elemento definido
+        alert('Em Ciências da Natureza estamos recentemente aprendendo os conteúdos de: ODS - É um apelo global criado pela ONU para acabar com a fome, pobreza, e a população possa viver em paz e prosperidade.  Mapas divisão territorial do Brasil - aprendemos sobre como são distribuídas as terras brasileiras.');
+    });
+
+    $('#btnMAT').click(function(){
+        //acao do clique no elemento definido
+        alert('ola pessoal');
+    });
+
+    $('#btnLIN').click(function(){
+        //acao do clique no elemento definido
+        alert('ola pessoal');
+    });
+
+    $('#btnTEC').click(function(){
+        //acao do clique no elemento definido
+        alert('ola pessoal');
+    });
+
+    $('#btnPP').click(function(){
+        //acao do clique no elemento definido
+        alert('ola pessoal');
+    });
+
+    $('#btnPV').click(function(){
+        //acao do clique no elemento definido
+        alert('ola pessoal');
+    });
+});
