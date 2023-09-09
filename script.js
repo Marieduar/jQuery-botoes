edb847b3c0e042cd428122ac2cbe3638fb1b8856
Maria Eduarda Aguirre <134699952+Marieduar@users.noreply.github.com>
2023-09-09 19:04:09 -0300

    ...

--- script.js
+++ script.js
@@ -8,31 +8,31 @@ $(document).ready(function() {
 
     $('#btnCH').click(function(){
         //acao do clique no elemento definido
-        alert('Em Ciências da Natureza estamos recentemente aprendendo os conteúdos de: ODS - É um apelo global criado pela ONU para acabar com a fome, pobreza, e a população possa viver em paz e prosperidade.  Mapas divisão territorial do Brasil - aprendemos sobre como são distribuídas as terras brasileiras.');
+        alert('Em Ciências Humanas estamos recentemente aprendendo os conteúdos de: ODS - É um apelo global criado pela ONU para acabar com a fome, pobreza, e a população possa viver em paz e prosperidade.  Mapas divisão territorial do Brasil - aprendemos sobre como são distribuídas as terras brasileiras.');
     });
 
     $('#btnMAT').click(function(){
         //acao do clique no elemento definido
-        alert('ola pessoal');
+        alert('Em Matemática estamos recentemente aprendendo os conteúdos de: Função Quadrática - é uma função polinomial associada a um polinômio do segundo grau. Função Exponencial - é aquela que a variável está no expoente e cuja base é sempre maior que zero e diferente de um.');
     });
 
     $('#btnLIN').click(function(){
         //acao do clique no elemento definido
-        alert('ola pessoal');
+        alert('Em Linguagens estamos recentemente aprendendo os conteúdos de: Português: Gênero Dramático e seus Subgêneros - são textos literários que tem o intuito de serem dramatizados ou encenados.                  Inglês: Futures Senses - são termos futures em inglês                         Artes: Curta Metragem - em um grupo os alunos devem escrever, encenar e editar um curta metragem.                                              Educação Física: Esportes - a cada aula o professor apresenta um tipo diferente de esporte para a turma.');
     });
 
     $('#btnTEC').click(function(){
         //acao do clique no elemento definido
-        alert('ola pessoal');
+        alert('Em Técnico estamos recentemente aprendendo os conteúdos de: Bootstrap - é um framework web que disponibiliza aplicações HTML, CSS e JavaScript. JQuery - o professor nos ensinou a utilizar a biblioteca do framework Bootstrap para interações entre elementos.');
     });
 
     $('#btnPP').click(function(){
         //acao do clique no elemento definido
-        alert('ola pessoal');
+        alert('Projeto Profissional: Cronograma de Estudos - os professores nos deram a tarefa de fazer um cronograma de estudos do nosso dia a dia. Site Empresa: em trios os alunos deveriam criar um site de uma empresa sustentável e mostrar para a turma.');
     });
 
     $('#btnPV').click(function(){
         //acao do clique no elemento definido
-        alert('ola pessoal');
+        alert('Projeto de Vida:  Entrevista - Em duplas os alunos deveriam simular uma entrevista de enprego em video e mostrar pra turma. Miniaulas - em trios os alunos escolhem um tema para fazer uma miniaula de 20-30 minutos.');
     });
 });
